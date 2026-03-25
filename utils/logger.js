0bbf5628863d3a55f08bc40f01bca21049d78e07
Sail3shp <saileshpokhrel557@gmail.com>
2026-03-25 13:35:07 +0545

    added winston logger

---
+++ utils/logger.js
@@ -0,0 +1,46 @@
+import { createLogger,transports,format } from "winston"; 
+import path from 'path'
+import fs from 'fs'
+import DailyRotateFile from "winston-daily-rotate-file";
+//import { DailyRotateFile } from "winston/lib/winston/transports";
+
+let dir = "logs"
+
+if(!dir) dir = path.resolve('logs')
+
+    if(!fs.existsSync(dir)){
+        fs.mkdirSync(dir)
+    }
+
+const logLevel = process.env.NODE_ENV === 'development'? 'debug':'warn'
+
+const dailyRotateFile = new DailyRotateFile({
+    level: logLevel,
+    filename: `${dir}/%DATE%-results.log`,
+    datePattern: "YYYY-MM-DD",
+    zippedArchive: true,
+    handleExceptions: true,
+    maxSize: '20m',
+    maxFiles: '14d',
+    format: format.combine(
+        format.errors({ stack: true }),
+        format.timestamp(),
+        format.json(),
+    )
+})
+
+export default createLogger({
+    transports:[
+        new transports.Console({
+            level: logLevel,
+            format: format.combine(
+                format.errors({stack:true}),
+                format.colorize(),
+                format.prettyPrint()
+            ),
+        }),
+        dailyRotateFile
+    ],
+    exceptionHandlers: [dailyRotateFile],
+    exitOnError: false,
+})
